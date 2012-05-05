4a31ea01032a2873cceaebcda5c59327b3edfc27
Ed F <crazyoatmeal@gmail.com>
2012-05-04 23:11:31 -0400

    add host parameter class

---
+++ AppFabricAppender/AppFabricAppenderHost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppFabricAppender
+{
+	public class AppFabricAppenderHost
+	{
+		public string Host { get; set; }
+		public string Port { get; set; }
+	}
+}
