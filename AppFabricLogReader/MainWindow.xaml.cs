04f8c15dead5cf786a3e3faa921382381341fc89
Ed F <crazyoatmeal@gmail.com>
2012-05-12 22:28:11 -0400

    update simple log reader to use config and have export to text

--- AppFabricLogReader/MainWindow.xaml.cs
+++ AppFabricLogReader/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.ApplicationServer.Caching;
+using Microsoft.Win32;
 
 namespace AppFabricLogReader
 {
@@ -21,18 +24,60 @@ namespace AppFabricLogReader
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private List<KeyValuePair<long, string>> logEntries;
 		public MainWindow()
 		{
 			InitializeComponent();
+			Loaded += MainWindow_Loaded;
+		}
 
-			DataCacheFactoryConfiguration factoryConfig = new DataCacheFactoryConfiguration();
-			List<DataCacheServerEndpoint> servers = new List<DataCacheServerEndpoint>();
-			servers.Add(new DataCacheServerEndpoint("127.0.0.1",22233));
-			factoryConfig.Servers = servers;
-			DataCacheFactory factory = new DataCacheFactory(factoryConfig);
-			DataCache cache = factory.GetCache("default");
-			List<KeyValuePair<string,object>> objects = cache.GetObjectsInRegion("EDWINPC").ToList();
-			Grid.ItemsSource = objects;
+		void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				string cacheName = ConfigurationManager.AppSettings["CacheName"] ?? "default";
+				string region = ConfigurationManager.AppSettings["LogRegion"];
+				DataCacheFactoryConfiguration config = new DataCacheFactoryConfiguration();
+				DataCacheFactory factory = new DataCacheFactory();
+				DataCache cache = factory.GetCache(cacheName);
+				List<KeyValuePair<string, object>> objects = cache.GetObjectsInRegion(region).ToList();
+
+				logEntries = new List<KeyValuePair<long, string>>();
+				for (int i = 0; i < objects.Count; i++)
+				{
+					long key;
+					if (long.TryParse(objects[i].Key, out key))
+					{
+						logEntries.Add(new KeyValuePair<long, string>(key, objects[i].Value.ToString()));
+					}
+				}
+				logEntries.Sort((a, b) => a.Key.CompareTo(b.Key));
+				Grid.ItemsSource = logEntries;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error getting objects from cache: " + ex.Message);
+			}
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			SaveFileDialog sfd = new SaveFileDialog();
+			sfd.FileName = "Logs"; // Default file name
+			sfd.DefaultExt = ".txt"; // Default file extension
+			sfd.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+			if (sfd.ShowDialog().GetValueOrDefault(false))
+			{
+				string fileName = sfd.FileName;
+				StreamWriter sw = new StreamWriter(fileName);
+				for (int i = 0; i < logEntries.Count; i++)
+				{
+					sw.Write(logEntries[i].Value);
+				}
+				sw.Flush();
+				sw.Close();
+			}
 		}
 	}
 }
